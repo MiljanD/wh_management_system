b727bb654a9f0e97e58c915a817b67bbddec3bdb
Miljan <m.duvnjak@outlook.com>
2025-07-14 20:14:11 +0200

    * Initial commit - DB creation, tables creation, connection to DB is done

---
+++ models/db.py
@@ -0,0 +1,14 @@
+import pymysql
+
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="dm3004^mk2606",
+    database="wh_management",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+if connection.open:
+    print("Database connected.")
+else:
+    print("Connection to database failed.")
